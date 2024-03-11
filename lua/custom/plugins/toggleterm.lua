6fb6de9c9d1cbce0cd9942d309ece0dfe440d206
Max <max.stolly@gmail.com>
2024-03-11 16:38:23 +0100

    Add float terminal

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,23 +1,3 @@
--- vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<CR>', { desc = 'Toggle terminal' })
--- vim.keymap.set('n', '<leader>tj', '<cmd>ToggleTerm direction=horizontal<CR>', { desc = 'Toggle terminal below' })
--- vim.keymap.set('n', '<leader>tl', '<cmd>ToggleTerm size=40 direction=vertical<CR>', { desc = 'Toggle terminal to right' })
-
--- function _Lazygit_toggle()
---   local Terminal = require('toggleterm.terminal').Terminal
---
---   if not Lazygit then
---     Lazygit = Terminal:new {
---       cmd = 'lazygit',
---       dir = 'git_dir',
---       hidden = true,
---     }
---   end
---
---   Lazygit:toggle()
--- end
-
--- vim.keymap.set('n', '<leader>g', '<cmd>lua _Lazygit_toggle()<CR>', { desc = 'Toggle lazygit' })
-
 return {
   'akinsho/toggleterm.nvim',
   opts = {},
@@ -25,15 +5,16 @@ return {
     require('toggleterm').setup()
 
     local Terminal = require('toggleterm.terminal').Terminal
-    local lazygit = Terminal:new { cmd = 'lazygit', hidden = true }
+    local lazygit = Terminal:new { cmd = 'lazygit', direction = 'float', hidden = true }
     function _Lazygit_toggle()
       lazygit:toggle()
     end
 
-    vim.keymap.set('n', '<leader>g', '<cmd>lua _Lazygit_toggle()<CR>', { desc = 'Toggle lazygit' })
+    vim.keymap.set('n', '<leader>gg', '<cmd>lua _Lazygit_toggle()<CR>', { desc = 'Toggle lazygit' })
 
-    vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<CR>', { desc = 'Toggle terminal' })
+    vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm<CR>', { desc = 'Toggle terminal' })
     vim.keymap.set('n', '<leader>tj', '<cmd>ToggleTerm direction=horizontal<CR>', { desc = 'Toggle terminal below' })
     vim.keymap.set('n', '<leader>tl', '<cmd>ToggleTerm size=40 direction=vertical<CR>', { desc = 'Toggle terminal to right' })
+    vim.keymap.set('n', '<leader>tf', '<cmd>ToggleTerm direction=float<CR>', { desc = 'Toggle floating terminal' })
   end,
 }
